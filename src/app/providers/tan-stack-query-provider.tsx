d6f27035a599f251836200e86c2778bf01216f49
백소원 <wish@baegsowon-ui-MacBookAir.local>
2024-10-07 20:45:57 +0900

    feat: 라이브러리 설치

---
+++ src/app/providers/tan-stack-query-provider.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+export function TanStackQueryProvider({ children }: React.PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+            retry: 0,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
