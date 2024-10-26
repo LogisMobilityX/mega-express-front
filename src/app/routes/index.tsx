eef4cb8af17a6cd39a40647164b85c45e1e394cd
백소원 <wish@baegsowon-ui-MacBookAir.local>
2024-10-26 21:12:59 +0900

    feat: 홈 UI  퍼블리싱

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,17 @@
 import { Components } from '@/pages/components'
+import { HomePage } from '@/pages/home/home-page'
 import { SignUpPage } from '@/pages/sign-up/ui/sign-up-page'
+import { HomeLayout } from '@/shared/ui/home-layout'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
   {
-    path: '/sign-up',
-    element: <SignUpPage />,
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: '/sign-up', element: <SignUpPage /> },
+    ],
   },
   {
     path: '/components',
--- src/shared/ui/home-layout.tsx
+++ src/shared/ui/home-layout.tsx
@@ -1,7 +1,12 @@
-interface Props {
-  children: React.ReactNode
-}
+import { Outlet } from 'react-router-dom'
 
-export const HomeLayout = ({ children }: Props) => {
-  return <div className='bg-[#EBF2FF]'>{children}</div>
+export const HomeLayout = () => {
+  return (
+    <div className='flex h-screen w-screen items-center justify-center bg-[#EBF2FF]'>
+      <div className='box-shadow-[0px_4px_140px_0px_rgba(0, 0, 0, 0.05)] h-[800px] w-[735px] rounded-bl-[30px] rounded-tl-[30px] bg-white/50'></div>
+      <div className='box-shadow-[0px_4px_140px_0px_rgba(0, 0, 0, 0.05)] h-[900px] w-[750px] rounded-[30px] bg-white'>
+        <Outlet />
+      </div>
+    </div>
+  )
 }
